738f4e44124bfb420447c6ba1e6856418d930c28
saadahmedbd <saadahmedbd0@gmail.com>
2024-09-15 23:50:49 +0600

    leetcode

---
+++ src/StackANDQueue/StackandQueue_implemantation.java
@@ -0,0 +1,14 @@
+package StackANDQueue;
+
+import java.util.Stack;
+
+public class StackandQueue_implemantation {
+    public static void main(String[] args) {
+
+    }
+    public static void stack (int nums[]){
+        Stack<Integer> stack =new Stack<>();
+        stack.push(2);
+        stack.push(3);
+    }
+}
