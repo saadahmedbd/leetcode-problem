0a4a6ad32e242acc0d2ca236bbda6a62414c1935
saadahmedbd <saadahmedbd0@gmail.com>
2024-10-12 23:57:17 +0600

    binary tree Implementation stack

--- src/Binary_tree/preOrder.java
+++ src/Binary_tree/preOrder.java
@@ -1,8 +1,12 @@
 package Binary_tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.TreeMap;
+
 public class preOrder {
     public static void main(String[] args) {
-
     }
     public static void PreOrder(buildTree.TreeNode root){
         if(root == null){
@@ -12,4 +16,24 @@ public class preOrder {
         PreOrder(root.left);
         PreOrder(root.right);
     }
+    public static List<Integer> preOrderStack(buildTree.TreeNode root){
+        List<Integer> list =new ArrayList<>();
+        if(root ==null) return list;
+        Stack<buildTree.TreeNode> stack=new Stack<>();
+        stack.push(root);
+        buildTree.TreeNode curr =root;
+        while(!stack.isEmpty()){
+            curr =stack.pop();
+            while(stack.isEmpty()){
+                list.add(curr.data);
+                if(curr.right != null){
+                    stack.push(curr.right);
+
+                curr =curr.left;
+                }
+            }
+        }
+        return list;
+    }
+
 }
