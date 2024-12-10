b8de51f8f287ceeecbad9454bd66c864badf8f55
saadahmedbd <saadahmedbd0@gmail.com>
2024-12-10 12:58:00 +0600

    subarray-question

---
+++ src/subarray_question/max_sum_of_subarray.java
@@ -0,0 +1,32 @@
+package subarray_question;
+
+public class max_sum_of_subarray {
+    public static void main(String[] args) {
+        int nums[]={1,5,4,2,9,9,9};
+        int k=3;
+        System.out.println(max_sum(nums,k));
+    }
+    public static  int max_sum(int nums[], int k){
+
+        int l=0;int r=0;
+        int sum=0;
+        int ans=0;
+        while (r<nums.length){
+            if(r-l+1 <k){
+                sum =sum + nums[r];
+                r++;
+            }else if(r-l+1 == k){
+
+                sum=sum+nums[r];
+                ans=Math.max(sum, ans);
+
+                sum=sum-nums[l];
+                l++;
+                r++;
+            }
+
+        }
+        return ans;
+
+    }
+}
---
+++ src/subarray_question/product_subarray.java
@@ -0,0 +1,22 @@
+package subarray_question;
+
+public class product_subarray {
+    public static void main(String[] args) {
+        int[] nums={2, 3};
+        System.out.println(maximum_product_subarray(nums));
+    }
+    // observation solution
+    public static int maximum_product_subarray(int nums[]){
+       int prefix=1, suffix=1;
+       int ans=Integer.MIN_VALUE;
+       int n= nums.length;
+       for(int i=0;i<n;i++){
+           if(prefix == 0) prefix=1;
+           if( suffix == 0) suffix =1;
+           prefix =prefix * nums[i];
+           suffix=suffix * nums[n-i-1];
+           ans=Math.max(ans, Math.max(suffix, prefix));
+       }
+       return ans;
+    }
+}
