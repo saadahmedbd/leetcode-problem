7c92112d0947a11894c0a419bd2f671cb5be2530
saadahmedbd <saadahmedbd0@gmail.com>
2024-12-20 12:36:00 +0600

    subarray-question

---
+++ src/subarray_question/find_k_different_integer.java
@@ -0,0 +1,32 @@
+package subarray_question;
+
+import java.util.HashMap;
+
+public class find_k_different_integer {
+    public static void main(String[] args) {
+        int [] nums={1,2,1,2,3};
+        int k=2;
+        System.out.println(findKDifferentNumber(nums, k));
+    }
+    public static int findKDifferentNumber(int nums[], int k){
+       return  slindingWindowAtMost(nums, k) - slindingWindowAtMost(nums, k-1);
+    }
+    public static int slindingWindowAtMost(int nums[], int distinct){
+        //create hashmap
+        HashMap<Integer, Integer> map =new HashMap<>();
+        int left=0, count=0;
+        for(int right=0;right<nums.length;right++){
+            map.put(nums[right], map.getOrDefault(nums[right], 0)+1);
+
+            while(map.size()>distinct){
+                map.put(nums[left], map.get(nums[left])-1);
+                if(map.get(nums[left]) == 0){
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            count += (right-left+1);
+        }
+        return count;
+    }
+}
---
+++ src/subarray_question/find_subarray.java
@@ -0,0 +1,30 @@
+package subarray_question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class find_subarray {
+    public static void main(String[] args) {
+        int[] nums={1,2,1,2,3};
+        System.out.println(findSubarray(nums));
+    }
+    public static int findSubarray(int nums[]){
+        int l=0;
+        int r=0;
+        int count=0;
+        int ans=0;
+        List<Integer> list=new ArrayList<>();
+        while(l<nums.length){
+            while (r<nums.length){
+                count++;
+                r++;
+            }
+            l++;
+            r=l;
+
+
+        }
+        return count;
+
+    }
+}
