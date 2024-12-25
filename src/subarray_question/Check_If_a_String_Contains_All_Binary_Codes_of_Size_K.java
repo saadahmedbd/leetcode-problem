2f20cef2d44951373d2b3cd2c76f18982a89ce8e
saadahmedbd <saadahmedbd0@gmail.com>
2024-12-25 12:30:53 +0600

    subarray-question

---
+++ src/subarray_question/Check_If_a_String_Contains_All_Binary_Codes_of_Size_K.java
@@ -0,0 +1,21 @@
+package subarray_question;
+
+import java.util.HashSet;
+
+public class Check_If_a_String_Contains_All_Binary_Codes_of_Size_K {
+    public static void main(String[] args) {
+        String s="00110110";
+        int k=2;
+        System.out.println(binaryCodeOfSizeK(s,k));
+    }
+//    00110110 substring is => 00, 01, 11, 10, 01,11,10
+//    loop find this substring and set remove duplicate value
+//    2^k == set.size return true or false
+    public static boolean binaryCodeOfSizeK(String s, int k){
+        HashSet<String> set =new HashSet<>();
+        for(int i=0;i<=s.length()-k;i++){
+            set.add(s.substring(i,i+k));
+        }
+        return (set.size() == Math.pow(2,k));
+    }
+}
---
+++ src/subarray_question/NumberofSubarrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.java
@@ -0,0 +1,32 @@
+package subarray_question;
+
+public class NumberofSubarrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold {
+    public static void main(String[] args) {
+        int []arr={5,6,7,1,2,3};
+        int k=2;
+        int threshold=3;
+        System.out.println(numOfSubarrays(arr,k,threshold));
+    }
+    public static int numOfSubarrays(int[] arr, int k, int threshold) {
+        int l=0;
+        int count=0;
+        double sum=0;
+        int r=0;
+        while(r<arr.length){
+            if(r-l+1 <k){
+                sum += arr[r];
+                r++;
+            }else if(r-l+1 == k){
+                sum += arr[r];
+                double average=sum/k;
+                if(average >= threshold){
+                    count ++;
+                }
+                sum -= arr[l];
+                l++;
+                r++;
+            }
+        }
+        return count;
+    }
+}
