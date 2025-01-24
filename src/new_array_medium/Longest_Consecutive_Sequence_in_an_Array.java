f41c91e1d993ba591774e3969cbd4ff4bb88c6d6
saadahmedbd <saadahmedbd0@gmail.com>
2025-01-24 14:47:57 +0600

    medium question

---
+++ src/new_array_medium/Longest_Consecutive_Sequence_in_an_Array.java
@@ -0,0 +1,29 @@
+package new_array_medium;
+
+import java.util.Arrays;
+
+public class Longest_Consecutive_Sequence_in_an_Array {
+    public static void main(String[] args) {
+        int [] nums={1,2,0,1};
+        System.out.println(longest_consecutive_sequence(nums));
+    }
+    public static int longest_consecutive_sequence(int [] nums){
+        Arrays.sort(nums);
+        int l=0, r=1;
+        int count=0;
+        int ans=0;
+        while(r< nums.length){
+            int subtract=nums[r]-nums[l];
+            if(subtract==1 || subtract==0){
+                count++;
+
+            }else{
+                count=0;
+            }
+           ans=Math.max(count, ans);
+            l++;
+            r++;
+        }
+        return ans+1;
+    }
+}
