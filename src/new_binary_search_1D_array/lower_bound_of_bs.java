1495f91b496742f5515299f880e5ca46c36f50c8
saadahmedbd <saadahmedbd0@gmail.com>
2025-02-24 10:38:58 +0600

    binary search

--- src/new_binary_search_1D_array/lower_bound_of_bs.java
+++ src/new_binary_search_1D_array/lower_bound_of_bs.java
@@ -2,22 +2,24 @@ package new_binary_search_1D_array;
 
 public class lower_bound_of_bs {
     public static void main(String[] args) {
-        int [] arr ={3,5,8,15,19};
-        int k=9;
+        int [] arr ={1,2,2,3};
+        int k=2;
         System.out.println(floor_of_target(arr, k));
     }
     public static int floor_of_target(int arr[], int k){
         int start =0;
         int end =arr.length-1;
+        int ans =-1;
         while(start <= end){
             int mid =start + (end-start)/2;
-            if(arr[mid] > k){
+            if(arr[mid] >= k){
+                ans=mid;
                 end =mid-1;
             }else{
                 start =mid +1;
             }
 
         }
-        return end;
+        return ans;
     }
 }
