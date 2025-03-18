9f95c0afe2b0e6b512dad1a690fe817cbc65cbf6
saadahmedbd <saadahmedbd0@gmail.com>
2025-03-18 13:23:04 +0600

    liked

--- src/new_1D_linked_List/FindCycle.java
+++ src/new_1D_linked_List/FindCycle.java
@@ -1,28 +1,28 @@
 package new_1D_linked_List;
 
-class  Node{
+class LLNode {
     int val;
-    Node next;
-    Node(){}
-    Node (int val, Node next){
+    LLNode next;
+    LLNode(){}
+    LLNode(int val, LLNode next){
         this.val=val;
         this.next=next;
     }
 }
 public class FindCycle {
     public static void main(String[] args) {
-        Node obj=new Node();
+        LLNode obj=new LLNode();
         obj.val=12;
         obj.val=13;
 
 
     }
-    public boolean findCycle(Node head){
+    public boolean findCycle(LLNode head){
 //        if(head == null || head.next == null){
 //            return ;
 //        }
-        Node fast=head;
-        Node slow=head;
+        LLNode fast=head;
+        LLNode slow=head;
         while (fast != null && fast.next != null){
             fast=fast.next.next;
             slow=slow.next;
