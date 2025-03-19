052a30ffefcb99779a3c082cf2d056869e3877f7
saadahmedbd <saadahmedbd0@gmail.com>
2025-03-19 12:08:30 +0600

    array

--- src/Daily_leetcode_challenge/Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I.java
+++ src/Daily_leetcode_challenge/Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I.java
@@ -12,11 +12,13 @@ public class Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I {
         for(int i=0;i<=nums.length-n;i++){
             if(nums[i] == 0){ //ex=[0,1,1,1,0,0]
             for(int j=i;j < i + n;j++){
-                    if(nums[j]==0){
-                        nums[j]=1;
-                    }else{
-                        nums[j]=0;
-                    }
+//                    if(nums[j]==0){
+//                        nums[j]=1;
+//                    }else{
+//                        nums[j]=0;
+//                    }
+//                istead use
+                nums[j]^=1; //toggle 0 to 1 and 1 to 0
                 }
                 count++;
             }
