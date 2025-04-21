7aed3aa6d33e2b311d87b24fa6b0a72e3a2962fe
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-21 23:58:45 +0600

    recursion pow

--- src/New_recursion_level_1/pow_x_n.java
+++ src/New_recursion_level_1/pow_x_n.java
@@ -3,9 +3,10 @@ package New_recursion_level_1;
 public class pow_x_n {
     public static void main(String[] args) {
         double x=2.00000;
-        int n=10;
+        int n=-2;
         System.out.println(pow(x,n));
         System.out.println(poww(x, n));
+        System.out.println(myPow(x,n));
     }
     public static double pow(double x, int n){
 
@@ -24,15 +25,33 @@ public class pow_x_n {
 
     }
     public static double poww(double x, int n){
-//        long number=Math.abs((long) n);
+        long number=Math.abs((long) n);
         double res=1;
-        if(n == 0){
-            return n< 0 ? 1/res : res;
+        if(n == 0  ){
+            return 1;
+        }
+
+        if (n < 0) {
+            return 1.0 / poww(x, -n);
         }
         if(n % 2== 1){
-            poww(res *x, n-1);
+            return x * poww(res *x, n-1);
+        }else{
+            return poww( x*x, n/2);
         }
-        poww(x*x,n/2);
-        return 0;
+
+    }
+    public static long myPow(double x, int n){
+        long number =Math.abs(n);
+        double res=1;
+        while (number != 0){
+            if(number %2 ==1){
+                res =res * x;
+                number =number -1;
+            }
+            x =x*x;
+            number=number/2;
+        }
+        return (long) (n< 0 ?1/res :res);
     }
 }
